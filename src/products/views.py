1fc15786b8fb5b825879f02b20f11014d654e71d
M.B.A. Fadeyi <fadeyibajide@gmail.com>
2023-08-16 18:49:40 +0100

    Product core
    Products' views, urls and template setup

--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .forms import ProductForm
+from .models import Product
+
+
+def product_create_view(request):
+    context = {}
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        if request.user.is_authenticated:
+            obj.user = request.user
+            obj.save()
+            return redirect("/products/create/")
+        form.add_error(None, "You must be logged in to create products")
+    context["form"] = form
+    return render(request, "products/create.html", context)
